9c18a91f0ce0a6ef160d767d194309b4eb248690
Kira Resari <kira@tri-tail.com>
2017-11-14 16:14:28 +0100

    Added Netfira Test Program

---
+++ Netfira Test Program/Netfira Test Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netfira_Test_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Netfira Test Program");
+
+            Console.ReadLine();
+        }
+    }
+}
